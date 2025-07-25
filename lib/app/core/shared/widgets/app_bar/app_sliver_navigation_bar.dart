29291de194e1e64c36ebe5c890dc8ddeb252765a
Marcoaudev <marcoau.dev@gmail.com>
2025-07-25 01:58:35 -0300

    Feat: remove characters image in app sliiver navigation bar

--- lib/app/core/shared/widgets/app_bar/app_sliver_navigation_bar.dart
+++ lib/app/core/shared/widgets/app_bar/app_sliver_navigation_bar.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:marvel_app/app/core/shared/images/app_images.dart';
 import 'package:marvel_app/app/core/shared/theme/colors/app_colors.dart';
 
 class AppSliverNavigationBar extends StatelessWidget {
@@ -27,18 +26,6 @@ class AppSliverNavigationBar extends StatelessWidget {
         title: Text(title, style: Theme.of(context).textTheme.titleMedium),
         pinned: true,
         backgroundColor: AppColors.primary,
-        flexibleSpace:
-            error!
-                ? null
-                : FlexibleSpaceBar(
-                  background: Container(
-                    color: const Color.fromARGB(230, 0, 0, 0),
-                    child: Image.asset(
-                      AppImages.allCharacters,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
       );
     }
   }
